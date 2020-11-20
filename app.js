9ff1ebdaf0cc57558bc08fa918245a97ae389cc6
timotheemcdonald <timotheemcdonald@gmail.com>
2020-11-19 23:29:42 -0500

    creating candy function

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const width = 8
+    const squares = []
 
-    
+    const candyColors = [
+       'red',
+       'blue',
+       'yellow',
+       'green',
+       'orange',
+       'purple'
+    ]
+
+    //Create Board
+    function createBoard() {
+        for (let i = 0; i < width*width; i++) {
+            const square = document.createElement('div')
+            let randomColor = Math.floor(Math.random() * candyColors.length)
+            square.style.backgroundColor = candyColors[randomColor]
+            grid.appendChild(square)
+            squares.push(square)
+        }   
+    }
+
+    createBoard()
 })
