ca4fb7348ed9109201796401425d769132207b21
timotheemcdonald <timotheemcdonald@gmail.com>
2020-11-20 00:50:11 -0500

    colors drop if empty space and board refills

--- app.js
+++ app.js
@@ -89,6 +89,22 @@ document.addEventListener('DOMContentLoaded', () => {
         squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced
     }
 
+    //drop candies once have some been cleared
+    function moveDown(){
+        for (i = 0; i < 55; i++){
+            if(squares[i + width].style.backgroundColor === ''){
+                squares[i + width].style.backgroundColor = squares[i].style.backgroundColor
+                squares[i].style.backgroundColor = ''
+                const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+                const isFirstRow = firstRow.includes(i)
+                if (isFirstRow && squares[i].style.backgroundColor === ''){
+                    let randomColor = Math.floor(Math.random() * candyColors.length)
+                    squares[i].style.backgroundColor = candyColors[randomColor]
+                }
+            }
+        }
+    }
+
     //Checking for Matches
     //Check for Row of Three
     function checkRowForThree() {
@@ -96,6 +112,9 @@ document.addEventListener('DOMContentLoaded', () => {
             let rowOfThree = [i, i+1, i+2]
             let decidedColor = squares[i].style.backgroundColor
             const isBlank = squares[i].style.backgroundColor === ''
+
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+            if (notValid.includes(i)) continue
 
             if(rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)){
                 score += 3
@@ -124,6 +143,7 @@ document.addEventListener('DOMContentLoaded', () => {
     checkColumnForThree()
 
     window.setInterval(function(){
+        moveDown()
         checkRowForThree()
         checkColumnForThree()
     }, 100)
